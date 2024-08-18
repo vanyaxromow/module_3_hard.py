eaafe256d6ea047af8da29be63dd79274e470cfc
vanyaxromow <vanya.xromow@yandex.ru>
2024-08-18 12:52:38 +0300

    18/08

---
+++ main.py
@@ -0,0 +1,28 @@
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+sum = 0
+tuple_ = (list, set, tuple)
+
+
+def calculate_structure_sum(*args):
+    # global sum
+    sum = 0
+    for i in args:
+        if isinstance(i, int):
+            sum += i
+        elif isinstance(i, str):
+            sum += len(i)
+        elif isinstance(i, tuple_):
+            sum += calculate_structure_sum(*i)
+        elif isinstance(i, dict):
+            sum += calculate_structure_sum(*i.items())
+    return sum
+
+
+result = calculate_structure_sum(data_structure)
+print(result)
